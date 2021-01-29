d679f99c6f7519b6a034cd594e410da601d8558e
mike <michaelwilsonsj@hotmail.com>
2021-01-28 20:24:28 -0500

    java variables lesson 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Lesson One Console App");
+        // data types
+        int studentAge = 17;
+        double gpa = 3.8;
+        char firstInitial = 'm';
+        char lastInitial = 'w';
+        boolean perfectAttendance = true;
+        System.out.println(studentAge);
     }
 }

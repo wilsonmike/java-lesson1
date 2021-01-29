c914a3c82140465e00b41fdfafdfe8fee6bbfe83
mike <michaelwilsonsj@hotmail.com>
2021-01-29 18:38:03 -0500

    concatenating strings

--- HelloWorld.java
+++ HelloWorld.java
@@ -12,5 +12,6 @@ public class HelloWorld {
         System.out.println(newString);
         char stringCharacter = newString.charAt(0);
         System.out.println(stringCharacter);
+        System.out.println(newString + " " + firstInitial);
     }
 }
